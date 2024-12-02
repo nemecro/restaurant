c24daa47bf3cd2525e2b0cb28aaa445620c046cc
nemecro <nemecstudies@gmail.com>
2024-12-02 17:57:02 +0100

    feat: create initial webpack setup with some dummy content

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: "html-loader",
+            },
+            {
+                test: /\.(svg, png. jpg, jpeg)$/i,
+                use: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: "./src/template.html",
+            },
+        ),
+    ],
+};
