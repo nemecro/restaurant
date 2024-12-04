99b9cae4c131eae847ea9bd82fcbb9ba821ca75f
nemecro <nemecstudies@gmail.com>
2024-12-04 15:36:32 +0100

    feat: write a module for rendering the menu content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { renderHome } from "./home.js";
+import {renderMenu} from "./menu.js";
 
 const content = document.querySelector('#content');
 
-// renderHome();
+renderHome();
+
