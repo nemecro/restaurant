0efaace2693a45ef1043cbab61a950392beb38e1
nemecro <nemecstudies@gmail.com>
2024-12-04 15:42:42 +0100

    feat: create clearContent function and add event listener to buttons, implement a logic to render Home or Menu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import { renderHome } from "./home.js";
 import {renderMenu} from "./menu.js";
 
 const content = document.querySelector('#content');
+const buttons = document.querySelectorAll('nav>button');
+
+const clearContent = function(){
+    content.innerHTML = '';
+};
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        clearContent();
+        if (button.id === 'home-btn'){
+            renderHome();
+        } else if (button.id === 'menu-btn'){
+            renderMenu();
+        }
+    });
+})
 
 renderHome();
 
